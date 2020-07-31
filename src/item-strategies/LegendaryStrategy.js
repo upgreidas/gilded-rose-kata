9354718b2317ac19370259360ca4e422ac56af3d
upgreidas <gungaudas@gmail.com>
2020-07-31 20:36:48 +0300

    Implement legendary item strategy

---
+++ src/item-strategies/LegendaryStrategy.js
@@ -0,0 +1,11 @@
+const BaseStrategy = require('./BaseStrategy');
+
+class LegendaryStrategy extends BaseStrategy {
+  updateQuality() {
+  }
+
+  updateSellIn() {
+  }
+}
+
+module.exports = LegendaryStrategy;
--- src/item-strategies/index.js
+++ src/item-strategies/index.js
@@ -1,9 +1,11 @@
 const BaseStrategy = require('./BaseStrategy');
 const IncreasingQualityStrategy = require('./IncreasingQualityStrategy');
 const BackstagePassStrategy = require('./BackstagePassStrategy');
+const LegendaryStrategy = require('./LegendaryStrategy');
 
 module.exports = {
   BaseStrategy: new BaseStrategy(),
   IncreasingQualityStrategy: new IncreasingQualityStrategy(),
   BackstagePassStrategy: new BackstagePassStrategy(),
+  LegendaryStrategy: new LegendaryStrategy(),
 };
